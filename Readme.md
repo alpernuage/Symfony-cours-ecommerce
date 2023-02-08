8feedf6694bd4a2dc5f102d9b7bacef7c829c7cc
alpernuage <alperakbulut@gmail.com>
2023-02-09 00:51:14 +0100

    add home page and Readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+🎨 Template Bootswatch (Lumen) : https://bootswatch.com/
+Icones FontAwesome : https://cdnjs.com/libraries/font-awesome
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,93 +2,19 @@
 
 namespace App\Controller;
 
-use App\Taxes\Calculator;
-use App\Taxes\Detector;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
-class HelloController extends AbstractController
+class HomeController extends AbstractController
 {
-    protected $twig;
-
-//    AbstractController fait déjà cette injection de $twig
-    public function __construct(Environment $twig)
-    {
-        $this->twig = $twig;
-    }
-//    protected $logger;
-//    protected $calculator;
-//
-//    public function __construct(LoggerInterface $logger, Calculator $calculator)
-//    {
-//        $this->logger = $logger;
-//        $this->calculator = $calculator;
-//    }
-
     /**
-     * @Route("/hello/{prenom?World2}", defaults={"prenom":"World1"}, name="hello", methods={"GET"})
+     * @Route("/", name="homepage")
      */
-//    public function hello($prenom = "World3", Detector $detector)
-    public function hello($prenom = "World3")
+    public function homepage()
     {
-//        $html = $twig->render('hello.html.twig', [
-//            'prenom' => $prenom,
-//            'ages' => [
-//                6,
-//                80,
-//                12,
-//                25
-//            ],
-//            'prenoms' => [
-//                'Alper',
-//                'Alp',
-//                'Er',
-//            ],
-//            'formateur1' => [
-//                'prenom' => 'Alper',
-//                'nom' => 'Bulut',
-//                'age' => 36,
-//            ],
-//            'formateur2' => [
-//                'prenom' => 'Marc',
-//                'nom' => 'Dupont',
-//                'age' => 45,
-//            ],
-//        ]);
-//        return new Response($html);
-
-//        dump($detector->detect(101, 50));
-//        dump($detector->detect(10, 5));
-//
-//        $tva = $this->calculator->calcul(100);
-//        dump($tva);
-//
-//        $this->logger->info("Mon message de log");
-//        return new Response("Hello $prenom");
-        return $this->render('hello.html.twig', ['prenom' => $prenom]);
-
+        return $this->render('home.html.twig');
     }
 
-    /**
-     * @Route("/example", name="example")
-     */
-    public function example()
-    {
-//        $html = $twig->render('example.html.twig', [
-//            'age' => 33
-//        ]);
-//        return new Response($html);
-        return $this->render('example.html.twig', ['age' => 36]);
-    }
-
-//  AbstractController contient déjà cette methode render()
-//    protected function render(string $path, array $variables = [])
-//    {
-//        $html = $this->twig->render($path, $variables);
-//        return new Response($html);
-//    }
 }
+
+
