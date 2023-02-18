f82ef3cc9e271f83c326bdab799eab5049e4574a
alpernuage <alperakbulut@gmail.com>
2023-02-19 00:46:19 +0100

    CRUD examples

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +13,33 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage(ProductRepository $productRepository)
+    public function homepage(EntityManagerInterface $em)
     {
-        $product = $productRepository->findOneBy(['price' => 1500]);
-        dump($product);
+        // CREATE
+/*         $product = new Product;
+        $product
+            ->setName('Table en métal')
+            ->setPrice(3000)
+            ->setSlug('table-en-métal');
+
+        $em->persist($product);
+        $em->flush();
+
+        dd($product); */
+
+        // UPDATE
+/*         $productRepository = $em->getRepository(Product::class);
+        $product = $productRepository->find(3);
+        $product->setPrice(2500);
+        $em->flush();
+        dd($product); */
+
+        // DELETE
+        $productRepository = $em->getRepository(Product::class);
+        $product = $productRepository->find(3);
+        $em->remove($product);
+        $em->flush();
+
         return $this->render('home.html.twig');
     }
-
 }
