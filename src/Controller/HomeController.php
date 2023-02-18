c41bd02ec1c4f9ded2114098f8dd253291cc1acd
alpernuage <alperakbulut@gmail.com>
2023-02-19 00:22:25 +0100

    Example with repository

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,11 +11,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function homepage()
+    public function homepage(ProductRepository $productRepository)
     {
+        $product = $productRepository->findOneBy(['price' => 1500]);
+        dump($product);
         return $this->render('home.html.twig');
     }
 
 }
-
-
