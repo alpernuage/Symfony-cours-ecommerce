cdb66f26646cbe2dc66aa6519d1adfbee145a5ee
alpernuage <alperakbulut@gmail.com>
2023-02-22 00:45:14 +0100

    install faker packages

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,11 +11,15 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker  = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
         for ($p = 0; $p < 100; $p++) {
             $product = new Product;
-            $product->setName("Produit n°$p")
-                ->setPrice(mt_rand(100, 200))
-                ->setSlug("produit-n-$p");
+            $product->setName($faker->productName)
+                ->setPrice($faker->price(4000, 20000))
+                ->setSlug($faker->slug());
 
             $manager->persist($product);
         }
