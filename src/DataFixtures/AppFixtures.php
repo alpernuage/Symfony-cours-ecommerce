04ccfe9f3f0e52dd592404b0d39824664cf05ce2
alpernuage <alperakbulut@gmail.com>
2023-02-21 23:32:03 +0100

    install faker php

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($p = 0; $p < 100; $p++) {
+            $product = new Product;
+            $product->setName("Produit n°$p")
+                ->setPrice(mt_rand(100, 200))
+                ->setSlug("produit-n-$p");
+
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }
