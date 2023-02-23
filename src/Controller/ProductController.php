644b0dce1fd0856ebe5fc5b74582e5ba541c19ea
alpernuage <alperakbulut@gmail.com>
2023-02-23 23:55:26 +0100

    discover some form methods in twig

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,13 +2,20 @@
 
 namespace App\Controller;
 
-use App\Repository\CategoryRepository;
+use App\Entity\Category;
 use App\Repository\ProductRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ProductController extends AbstractController
 {
@@ -47,6 +54,56 @@ class ProductController extends AbstractController
 
         return $this->render('product/show.html.twig', [
             'product' => $product,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/product/create", name="product_create")
+     */
+    public function create(FormFactoryInterface $factory)
+    {
+        $builder = $factory->createBuilder();
+
+        $builder->add('name', TextType::class, [
+            'label' => 'Nom du produit',
+            'attr' => [
+                'class' => 'form-control',
+                'placeholder' => 'Tapez le nom du produit'
+            ],
+        ])
+            ->add('shortDescription', TextareaType::class, [
+                'label' => 'Description courte',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Tapez une description assez courte mais parlante pour le visiteur'
+                ],
+            ])
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix du produit',
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'Tapez le prix du produit en €'
+                ],
+            ])->add('category', EntityType::class, [
+            'label' => 'Catégorie',
+            'attr' => [
+                'class' => 'form-control',
+            ],
+            'placeholder' => '-- Choisir une catégorie --',
+            'class' => Category::class,
+            'choice_label' => function(Category $category){
+                return strtoupper($category->getName());
+            }
+        ]);
+
+        // $builder->setMethod('GET')
+        //     ->setAction('/toto');
+
+        $form = $builder->getForm();
+        $formView = $form->createView();
+
+        return $this->render('product/create.html.twig', [
+            'formView' => $formView
         ]);
     }
 }
