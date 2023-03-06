9513691fdd637e03e3b20986c423ead976588197
alpernuage <alperakbulut@gmail.com>
2023-03-06 13:53:12 +0100

    examples with flash bag

---
+++ src/Controller/CartController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/cart/add/{id}", name="cart_add", requirements={"id":"\d+"})
+     */
+    public function add($id,ProductRepository $productRepository, SessionInterface $session): Response
+    {
+        // 0. Securisation : est-ce que le produit existe ?
+        $product = $productRepository->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException("Le pdoruit $id n'existe pas !");
+        }
+
+        // 1. Retrouver le panier dans la session (sous forme de tableau)
+        // 2. S'il n'existe pas encore, alors prendre un tableau vide
+        $cart = $session->get('cart', []);
+
+        // 3. Voir si le produit ($id) existe déjà dans le tableau
+        // Ex tableau: [12 => 4, 29 => 3]
+        // 4. Si c'est le cas, simplement augmenter la quantité
+        // 5. Sinon, ajouter le produit avec la quantité 1
+        if (array_key_exists($id, $cart)) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        // Enregistrer le tableau mis à jour dans la session
+        $session->set('cart', $cart);
+
+        /** @var FlashBag */
+        $flashBag = $session->getBag('flashes');
+        $flashBag->add('success', "tout s'est bien passé");
+        $flashBag->add('warning', "Attention");
+
+        dump($flashBag->get('success'));
+
+        dd($flashBag);
+        
+        dd($session->getBag('flashes')); // flashName
+        // dd($session->getBag('attributes')); // attributeName
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug()
+        ]);
+    }
+}
